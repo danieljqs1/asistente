9e89c6ba9b35e54637d4c7c991352e2ba76d310e
danieljqs1 <daniel_jacques@portalnuva.com>
2024-08-13 14:08:59 -0700

    Nuevo inicio del proyecto

--- state.py
+++
@@ -1,8 +0,0 @@
-import operator
-from typing import Annotated, TypedDict, Union
-from langgraph.graph.message import add_messages
-from langchain_core.agents import AgentAction, AgentFinish
-from langchain_core.messages import BaseMessage
-
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
